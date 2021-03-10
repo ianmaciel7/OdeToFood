8b6de366d3cf1a06082bc14a320af21a6889f5a8
Ian <ianmaciel76@gmail.com>
2021-03-10 11:00:54 -0300

    Adicionando RazorRuntimeCompilation

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -25,7 +25,8 @@ namespace OdeToFood
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IRestaurantData,InMemoryRestaurantData>();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+      
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
