d7408bd2411d1cada48a7ff4e3274b211cde1681
Ian <ianmaciel76@gmail.com>
2021-03-24 15:18:14 -0300

    Corrigindo InMemoryRestaurantData

--- OdeToFood.Data/InMemoryRestaurantData.cs
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -1,32 +1,22 @@
-﻿using OdeToFood.Core;
+﻿using System.Linq;
+using OdeToFood.Core;
 using System.Collections.Generic;
-using System.Linq;
+
 
 namespace OdeToFood.Data
 {
     public class InMemoryRestaurantData : IRestaurantData
     {
         readonly List<Restaurant> restaurants;
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-            return newRestaurant;
-        }
 
-        public int Commit()
+        public InMemoryRestaurantData()
         {
-            throw new System.NotImplementedException();
-        }
-
-        public Restaurant Delete(int id)
-        {
-            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
-            if (restaurant != null)
+            restaurants = new List<Restaurant>()
             {
-                restaurants.Remove(restaurant);
-            }
-            return restaurant;
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location="London", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine=CuisineType.Mexican}
+            };
         }
 
         public Restaurant GetById(int id)
@@ -34,18 +24,11 @@ namespace OdeToFood.Data
             return restaurants.SingleOrDefault(r => r.Id == id);
         }
 
-        public int GetCountOfRestaurants()
+        public Restaurant Add(Restaurant newRestaurant)
         {
-            return restaurants.Count;
-        }
-
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                   orderby r.Name
-                   select r;
-
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
         }
 
         public Restaurant Update(Restaurant updatedRestaurant)
@@ -58,6 +41,34 @@ namespace OdeToFood.Data
                 restaurant.Cuisine = updatedRestaurant.Cuisine;
             }
             return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+            return null;
+        }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
         }
     }
 }
